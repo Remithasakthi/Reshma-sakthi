b4309afb2a24f4f18c8cc5143a48ab0c8853a076
Remithasakthi <43408064+Remithasakthi@users.noreply.github.com>
2018-12-13 11:10:19 +0530

    Create register.js

---
+++ register.js
@@ -0,0 +1,37 @@
+function validateform(){
+x=document.get Element By Id("Username")value;
+if(x==""){
+alert("Username must be filled");
+return false;
+}
+x=document.get Element By Id("Email")value;
+if(x==""){
+alert("Email must be filled");
+return false;
+}
+x=document.get Element By Id("Password")value;
+if(x==""){
+alert("Password number must be filled");
+return false;
+}
+x=document.get Element By Id("Degree")value;
+if(x==""){
+alert("Degree must be filled");
+return false;
+}
+x=document.get Element By Id("Registernumber")value;
+if(x==""){
+alert("Register number must be filled");
+return false;
+}
+x=document.get Element By Id("Branch")value;
+if(x==""){
+alert("Branch must be filled");
+return false;
+}
+x=document.get Element By Id("College name")value;
+if(x==""){
+alert("College name must be filled");
+return false;
+}
+}
